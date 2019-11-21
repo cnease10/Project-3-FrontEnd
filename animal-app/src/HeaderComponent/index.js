78f4761062a36adfb017c3777f6b9c34c2a38bef
malloryclark93 <mallory_clark@live.com>
2019-11-21 10:14:51 -0700

    mallory

--- animal-app/src/HeaderComponent/index.js
+++ animal-app/src/HeaderComponent/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {Header, List} from 'semantic-ui-react'
+import './headercomponent.css'
+
 
 //functional / show only component that will be at the
 //top of every page
 const HeaderComponent = () => {
     return (
         <Header>
+                <div class="header1">
             <List>
                 <List.Item>
-                    <Link to='/'>Home</Link>
+                    <Link to='/'>Home</Link> 
                 </List.Item>
                 <List.Item>
                     <Link to="/shelters">Shelters</Link>
                 </List.Item>
             </List>
+            </div>
         </Header>
     )
 }
--- animal-app/src/HomeComponent/index.js
+++ animal-app/src/HomeComponent/index.js
@@ -1,23 +1,59 @@
 import React from 'react'
-import {Grid} from 'semantic-ui-react'
+import {Grid, Segment} from 'semantic-ui-react'
+import './homecomponent.css'
 
+
+   
 const HomeComponent = () => {
     return(
-        <Grid textAlign='center' >
-        <Grid.Row>
-            <h1>Animal Allies</h1>
+        <body>
+     <Grid>
+      <Grid.Row>
+        // <div class="text">
+        //     <h1>Animal Allies</h1>
+        //     </div>
+        
+
+                
         </Grid.Row>
-        <Grid.Row>
-            image imgage
-        </Grid.Row>
-        <Grid.Row>
-            image image
-        </Grid.Row>        
-        <Grid.Row>
-            copyright
-        </Grid.Row>
-        </Grid>
+     </Grid>
+ </body>
+
     )
 }
 
+// import Cat from './cat1.png'
+//  <div class="img">
+//                 <img src={Cat}/>
+//             </div>
+   
 export default HomeComponent
+
+
+
+ 
+
+        // <Grid.Row>
+      //       <h1>Animal Allies</h1>
+      //   </Grid.Row>
+      //   <Grid.Row>
+      //       <Segment>1</Segment>
+      //        <Grid.Column width={8}>
+      //       <img src={Cat}/>
+      //       </Grid.Column>
+      //    </Grid.Row>
+      //    <Grid.Row>
+      //        <Segment>2</Segment>
+      //        <img src={Dog}/>       
+      //   </Grid.Row>
+      //   <Grid.Row>
+      //       <Segment>3</Segment>
+      //       <img src={Cat2}/>
+      //   </Grid.Row>
+      //   <Grid.Row>
+      //       <Segment>4</Segment>
+      //       <img src={Dog2}/> 
+      //    </Grid.Row>  
+      //   <Grid.Row>
+      //       copyright
+      //   </Grid.Row>
